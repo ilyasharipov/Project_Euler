c0bb31d1f30761672af765e692b2845dfd09cf07
1_Hours <sharipov.yanepenb@gmail.com>
2019-03-12 18:35:40 +0700

    new decide

---
+++ Largest_prime_factor/index.js
@@ -0,0 +1,44 @@
+//Простые делители числа 13195 - это 5, 7, 13 и 29.
+
+//Каков самый большой делитель числа 600851475143, являющийся простым числом?
+
+//Функция для нахождения простого числа
+// Простые делители числа 13195 - это 5, 7, 13 и 29.
+
+// Каков самый большой делитель числа 600851475143, являющийся простым числом?
+
+// Функция для нахождения простого числа
+const isPrime = (num) => {
+    if (num <= 1) {
+      return false;
+    }
+  
+    const iter = (counter, divisor) => {
+      if (counter === divisor) {
+        return `This number - ${num} is a prime.`;
+      } else if (counter % divisor === 0) {
+        return `This number - ${num} is not a prime.`;
+      }
+      return iter(num, divisor + 1);
+    };
+    return iter(num, 2);
+  };
+  
+  // Вычисление всех делителей числа
+  const divisorsOfNumber = (num) => {
+    const iter = (counter, divisor, acc) => {
+      if (divisor > counter) {
+        return acc;
+      }
+      if (counter % divisor === 0) {
+        acc.push(divisor);
+        counter /= divisor; // Делим сразу же, что бы уменьшить заданное число
+      }
+      return iter(counter, divisor + 1, acc.slice());
+    };
+    return iter(num, 2, []);
+  };
+  
+  divisorsOfNumber(600851475143)
+    .map(n => isPrime(n))
+    .pop();
